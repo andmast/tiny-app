a0dcfadf92f84d9cd254094553c3eeae2c8ed540
Andrea <anmastrantoni@gmail.com>
2019-04-12 16:49:07 +0000

    frist execerise completed

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-ar express = require("express");
+var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
@@ -7,10 +7,19 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//------------GETSSSSS----------------------
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+//-----------------------------------------------
