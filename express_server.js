1662d3070a52ce95511964ecdbdf352ea1299491
Andrea <anmastrantoni@gmail.com>
2019-04-12 23:49:41 +0000

    added urls data base

--- express_server.js
+++ express_server.js
@@ -49,6 +49,10 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
 
 //^^^^^^^^^^^^^^^^^^Variables^^^^^^^^^^^^^^^^^^
 
