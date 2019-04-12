781c519aa6ce7aefffd529bdd483f210ec1e3b6f
Andrea <anmastrantoni@gmail.com>
2019-04-12 17:06:06 +0000

    added index page and get route with template vars

--- express_server.js
+++ express_server.js
@@ -20,7 +20,23 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req,res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
+});
+//-----------------------------------------------
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-//-----------------------------------------------
+
+
+
+
+
+
+
+
+
