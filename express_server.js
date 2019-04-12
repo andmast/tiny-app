9b3cbdaf8724e3191f4290f845b8fb55a8d435aa
Andrea <anmastrantoni@gmail.com>
2019-04-12 17:12:05 +0000

    added urls show page and route

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//------------GETSSSSS----------------------
+//------------GETS----------------------
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,6 +24,13 @@ app.get("/urls", (req,res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+
 //-----------------------------------------------
 
 
